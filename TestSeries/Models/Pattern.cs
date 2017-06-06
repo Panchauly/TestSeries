dfc3c36b75366edb973bf8414631e7078fa087bb
Amit <Panchauly@gmail.com>
2017-06-06 18:00:00 +0530

    New Model Update

--- TestSeries/Models/Pattern.cs
+++ TestSeries/Models/Pattern.cs
@@ -1,11 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace TestSeries.Models
 {
     public class Pattern
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int PatternId { get; set; }
+
+        public int Branch { get; set; }
+        public int Level { get; set; }
+        public int Subject { get; set; }
+        public int Chapter { get; set; }
+
+        [ForeignKey("Branch")]
+        public virtual Branch Branches { get; set; }
+
+        [ForeignKey("Level")]
+        public virtual Level Levels { get; set; }
+
+        [ForeignKey("Subject")]
+        public virtual Subject Subjects { get; set; }
+
+        [ForeignKey("Chapter")]
+        public virtual Chapter Chapters { get; set; }
+
     }
 }
---
+++ TestSeries/Models/Question.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TestSeries.Models
+{
+    public class Question
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int QuestionId { get; set; }
+        public string QuestionBody { get; set; }
+        public string A { get; set; }
+        public string B { get; set; }
+        public string C { get; set; }
+        public string D { get; set; }
+        public char RightAnswer { get; set; }
+        public bool AnyImage { get; set; }
+        public int DificultyLevel { get; set; }
+        public int QuestionImage { get; set; }
+        public int UploadedBy { get; set; }
+        public DateTime UploadedOn { get; set; }
+        public int Pattern { get; set; }
+
+        [ForeignKey("QuestionImage")]
+        public virtual Image Image { get; set; }
+
+        [ForeignKey("UploadedBy")]
+        public virtual InstituteProfile Institute { get; set; }
+
+        [ForeignKey("Pattern")]
+        public virtual Pattern Patterns { get; set; }
+    }
+}
