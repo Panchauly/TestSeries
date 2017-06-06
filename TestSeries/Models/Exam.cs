53f5db1362e291e1277e4fba12ef6a84683cedf8
Amit <Panchauly@gmail.com>
2017-06-06 22:04:11 +0530

    new changes

---
+++ TestSeries/Models/Exam.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TestSeries.Models
+{
+    public class Exam
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ExamId { get; set; }
+        public int Time { get; set; }
+        public int Questions { get; set; }
+        public DateTime ExamDate { get; set; }
+        public int Institute { get; set; }
+        public int Marks { get; set; }
+        public int NegativeMarks { get; set; }
+        public int Pattern { get; set; }
+
+        [ForeignKey("Institute")]
+        public virtual InstituteProfile Instituteprofile { get; set; }
+
+        [ForeignKey("Pattern")]
+        public virtual Pattern Patterns { get; set; }
+    }
+}
---
+++ TestSeries/Models/Result.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TestSeries.Models
+{
+    public class Result
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ResultId { get; set; }
+        public int Exam { get; set; }
+        public string Student { get; set; }
+        public string Answers { get; set; }
+        public int RightAnswers { get; set; }
+        public int WrongAnswers { get; set; }
+
+        [ForeignKey("Student")]
+        public virtual Student Students { get; set; }
+
+        [ForeignKey("Exam")]
+        public virtual Exam Exams { get; set; }
+
+    }
+}
