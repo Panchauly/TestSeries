aa2782bccd6926b13079936a0aff1843e7042002
Amit <Panchauly@gmail.com>
2017-06-10 11:16:42 +0530

    New Changes

---
+++ TestSeries/Controllers/StudentController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TestSeries.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public new ActionResult Profile()
+        {
+            return View();
+        }
+
+        public ActionResult Exam()
+        {
+            return View();
+        }
+
+    }
+}
